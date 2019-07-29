a35b3885bcf26395260f50c75bcaafafd51eba3a
xiongkunlun <38325019+xiongkunlun@users.noreply.github.com>
2019-07-29 18:28:23 +0800

    fix error

--- gene/main/src/main/java/com/icecore/main/Test.java
+++ gene/main/src/main/java/com/icecore/main/Test.java
@@ -28,7 +28,7 @@ public class Test {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://192.168.31.31:3306/welcome_student?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://localhost:3306/welcome_student?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
